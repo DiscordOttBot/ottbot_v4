457177b71420a055d70a34e89038b223849ef550
AlexanderHOtt <alex@the3bears.org>
2021-10-02 22:21:23 -0700

    type config class

--- ottbot/config.py
+++ ottbot/config.py
@@ -9,16 +9,18 @@ import dotenv
 
 dotenv.load_dotenv()
 
+SupportedTypes = t.Union[bool, int, float, str, set[t.Any]]
+
 
 class ConfigMeta(type):
     """Metaclass for extracting environment variables"""
 
-    def _resolve_value(cls, value: str):
+    def _resolve_value(cls, value: str) -> SupportedTypes:
         """Type casts values from a string
         Example:
             str:Discord_bot_token -> "Discord_bot_token"
         """
-        _types: dict[str, t.Callable[..., t.Any]] = {
+        _types: dict[str, t.Callable[..., SupportedTypes]] = {
             "bool": bool,
             "int": int,
             "float": float,
@@ -28,29 +30,30 @@ class ConfigMeta(type):
         }
         return _types[(v := value.split(":", maxsplit=1))[0]](v[1])
 
-    def _resolve_key(cls, key: str) -> t.Callable[..., t.Any]:
+    def _resolve_key(cls, key: str) -> SupportedTypes:
         """Reads environment variable from machine or .env file. In case of type casting, it will call cls._resolve_value(key)"""
         try:
             return cls._resolve_key(environ[key])
         except Exception:  # value contains ":" and needs to be type casted
             return cls._resolve_value(key)
 
-    def __getattr__(cls, name: str):
+    def __getattr__(cls, name: str) -> SupportedTypes:
         try:
             return cls._resolve_key(name)
         except KeyError:
             traceback.print_exc()
             raise AttributeError(f"{name} is not a key in config.") from None
 
-    def __getitem__(cls, name: str):
+    def __getitem__(cls, name: str) -> SupportedTypes:
         return cls.__getattr__(name)
 
 
 class Config(metaclass=ConfigMeta):
-    """Class for accessing environment variables on the machine or .env file.
+    """
+    Class for accessing environment variables on the machine or .env file.
     Examples:
     `Config.TOKEN`
     `Config["TOKEN"]`
     """
 
-    pass
+    ...
