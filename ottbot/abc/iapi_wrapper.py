a876047b8bd08c1291479b2c69555ebbe54fb436
AlexanderHOtt <aott01@protonmail.ch>
2021-10-27 18:56:03 -0700

    add api wrapper interface

---
+++ ottbot/abc/iapi_wrapper.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod, abstractproperty
+from fastapi import APIRouter
+from ottbot.core.bot import OttBot
+
+
+class IAPIWrapper(ABC):
+    @abstractmethod
+    def __init__(self, routers: list[APIRouter]) -> None:
+        self.__bot
+        self.__routers
+
+    @abstractproperty
+    def bot(self) -> OttBot:
+        """Bot property for dynamic type checking"""
+
+    @bot.setter
+    def set_bot(self, bot: OttBot) -> None:
+        """Bot setter for dynamic type checking"""
+
+    @abstractproperty
+    def routers(self) -> list[APIRouter]:
+        """Routers property for dynamic type checking"""
+
+    @routers.setter
+    def set_data(self, routers: list[APIRouter]) -> None:
+        """Routers setter for dynamic checking"""
